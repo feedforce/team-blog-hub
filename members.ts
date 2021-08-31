bc75bda8cced7608f038b10ad58a6c616fee9e95
Yoko TAMADA <tamada@feedforce.jp>
2021-08-31 14:54:55 +0900

    Add @tmd45 to members

--- members.ts
+++ members.ts
@@ -15,6 +15,19 @@ export const members: Member[] = [
   //   websiteUrl: "optional",
   // },
   {
+    name: "tmd45",
+    role: "SocialPLUS CS & Sr.Developer",
+    bio: "散財部風紀取り乱し委員わいわい係",
+    avatarSrc: "https://www.gravatar.com/avatar/f58db7375362f804258f3cb3476b1586?size=256",
+    sources: [
+      "https://developer.feedforce.jp/feed/author/tmd45",
+    ],
+    includeUrlRegex: "developer.feedforce.jp|blog.tmd45.jp",
+    twitterUsername: "tmd45",
+    githubUsername: "tmd45",
+    websiteUrl: "https://tmd45.jp",
+  },
+  {
     name: "tsub",
     bio: "コンテナが好きです",
     avatarSrc: "https://www.gravatar.com/avatar/cb16e623d2ac0c0d10440745d4525f04?size=256",
